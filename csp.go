b959336c2a1731bb509e3db12c4a05ba082669df
Michael van der Loo <michaeljvanderloo@gmail.com>
2022-10-17 15:47:27 -0400

    update interfaces

--- csp.go
+++ csp.go
@@ -2,37 +2,42 @@ package main
 
 /**
  * Value for the CSP
- * For sudoku this is limited to integers, which allow for comparison, ordering, and arithmatic
+ * For sudoku this is limited to integers,
+ * which allow for comparison, ordering, and arithmatic operations
  **/
 type Value interface {
-  Integer
+	int8 | uint8 | int16 | uint16 | int32 | uint32 | int64 | uint64
 }
 
 /**
  * Variable for the CSP
+ * Holds an optional value and a domain of
  **/
-type Variable interface {
-  assign(Value)
-  getDomain() []Value
-  removeVal(Value)
+type Variable[V Value] interface {
+	assign(V)
+	getDomain() []V
+	removeVal(V)
 }
 
-/**
- * Different types of constraints for the CSP
- **/
-type Constraint_t[T comparable] interface {
-  type T
+type Constraint_t interface {
+	comparable
 }
+
+const (
+	EQUAL int8 = iota
+	NOT_EQUAL
+	SUM_TO
+)
 
 /**
  * Constraint for the CSP
  **/
-type Constraint[T any] struct {
-  type Constraint_t
-  constrained []*Variables
+type Constraint[C Constraint_t, V Value, X Variable[V]] struct {
+	constraint  C
+	constrained []*X
 }
 
-type CSP interface {
-  getVariables() []Variable
-  getConstraints() []Constraints
+type CSP[V Value, X Variable[V], C Constraint_t] interface {
+	getVariables() []X
+	getConstraints() []Constraint[C, V, X]
 }
